c1aedd9cc9010c27881f3c8eb8a8c5f921d76cdf
Shallower <83507659+Frank17@users.noreply.github.com>
2022-04-25 15:38:47 -0700

    Update errors.py

--- mager/gmail_manager/errors.py
+++ mager/gmail_manager/errors.py
@@ -1,4 +1,12 @@
 class InvalidPathError(Exception):
     def __init__(self, path):
-        self.message = f'{path} is not an URL nor an existing path to a file'
+        self.message = f'{path} is not a valid URL to an image source nor an existing '
+                        'path to an image file'
+        super().__init__(self.message)
+
+        
+class UnmatchedImageNumberError(Exception):
+    def __init__(self, temp_img_n, list_img_n):
+        self.message = f'Image number found in template ({temp_img_n}) is different ' \
+                       f'than the number of image paths ({list_img_n}) in the list'
         super().__init__(self.message)
